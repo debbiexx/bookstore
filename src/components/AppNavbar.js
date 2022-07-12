af8560d1029f708a6bec5c6c436c3ac9d5e4d049
Debbie Joy <rosales.debbiejoy@nexseed.net>
2022-07-12 16:01:05 +0800

    working with navlinks

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -16,13 +16,13 @@ export default function AppNavbar () {
         <Nav.Link as ={Link} to="/"><img src={logo} alt="logo" className='navbar-logo'/></Nav.Link>
         <ul className='navbar-nav align-items-center'>
             <li className='m-auto'>
-                <small className='text-brand'>Welcome to our bookstore!</small>
+                <small className='navbar-text'>Welcome to our bookstore!</small>
             </li>
         </ul>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/" className='text-brand'>
+            <Nav.Link as={Link} to="/" className='navbar-text'>
                     <ButtonCart>
                         <span className='mx-2'>
                         <i className="fa fa-cart-plus" />
@@ -31,18 +31,18 @@ export default function AppNavbar () {
                     </ButtonCart>
                     </Nav.Link>
                     
-                <Nav.Link as={Link} to="/" className='text-brand'>
+                <Nav.Link as={Link} to="/" className='navbar-text'>
                     <ButtonCart>
                     Categories&nbsp;
                     </ButtonCart>
                     </Nav.Link>
-                    <Nav.Link as={Link} to="/" className='text-brand'>
+                    <Nav.Link as={Link} to="/" className='navbar-text'>
                     <ButtonCart>
                     Best Sellers&nbsp;
                     </ButtonCart>
                     </Nav.Link>
                    
-                    <Nav.Link as={Link} to="/register" className='text-brand'>
+                    <Nav.Link as={Link} to="/register" className='navbar-text'>
                             <ButtonSign>&nbsp;SIGNUP&nbsp; </ButtonSign></Nav.Link>	
                 
                     
